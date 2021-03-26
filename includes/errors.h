341fccdbe84bc4eae73372a9990df06d9d44316f
appinha <amanda_pinha@hotmail.com>
2021-03-25 23:20:44 -0300

    [errors] Adds errors module

---
+++ includes/errors.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 22:56:35 by apuchill          #+#    #+#             */
+/*   Updated: 2021/03/25 22:57:19 by apuchill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef ERRORS_H
+# define ERRORS_H
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              HEADERS
+*/
+# include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft.h"
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              MACROS
+*/
+# define SYSERR		"000"
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              FUNCTION PROTOTYPES
+*/
+/*
+** FILE: errors.c
+*/
+void			error_msg_and_exit(char *code);
+char			*get_error_code(short int categ, short int elem_id,
+						short int err_id);
+/*
+** FILE: utils.c
+*/
+void			*malloc_ver(size_t size);
+int				open_ver(char *file);
+void			close_ver(int fd);
+
+#endif
---
+++ srcs/errors/errors.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 22:58:19 by apuchill          #+#    #+#             */
+/*   Updated: 2021/03/25 23:06:57 by apuchill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "errors.h"
+
+char		*get_error_code(short int category, short int elem_id,
+							short int err_id)
+{
+	char	*code;
+
+	code = malloc_ver(4 * sizeof(char));
+	code[0] = category + '0';
+	code[1] = elem_id + '0';
+	code[2] = err_id + '0';
+	code[3] = '\0';
+	return (code);
+}
+
+static void	print_error_msg(char *code)
+{
+	int		fd;
+	char	*msg;
+
+	fd = open_ver("srcs/errors/errors.txt");
+	while (get_next_line(fd, &msg) == 1)
+	{
+		if (ft_strncmp(code, msg, 3) == 0)
+		{
+			ft_putstr_fd("\033[1m\033[1;31mError\033[0m\n", STDERR_FILENO);
+			ft_putstr_fd(msg, STDERR_FILENO);
+			ft_putchar_fd('\n', STDERR_FILENO);
+			break ;
+		}
+		free(msg);
+	}
+	free(msg);
+	close_ver(fd);
+}
+
+void		error_msg_and_exit(char *code)
+{
+	if (errno && ft_strcmp(code, SYSERR) == 0)
+		perror("\033[1m\033[1;31mSystem error\033[0m");
+	else if (code != SYSERR)
+		print_error_msg(code);
+	else
+		ft_putstr_fd("Fatal Error\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
