bcc9cc6683079ef673f55ba2b8fca1b23a5b247a
appinha <amanda_pinha@hotmail.com>
2021-03-25 23:22:04 -0300

    Very very basic shell structure; prints prompt (one time only)

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 11:23:43 by apuchill          #+#    #+#             */
-/*   Updated: 2021/03/21 11:26:05 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/03/25 23:16:36 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,23 @@
 ** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
 **                              HEADERS
 */
+# include <stdbool.h>
+# include <limits.h>
 # include "libft.h"
+# include "errors.h"
 
 /*
 ** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
 **                              MACROS
 */
-# define NAME			"miniSHELL"
+# define C_END		"\033[0m"
+# define C_BOLD		"\033[1m"
+# define C_PINK		"\033[38;5;13m"
+# define C_BLUE		"\033[38;5;75m"
+# define C_MAGENTA	"\033[38;5;199m"
+# define C_GREEN	"\033[1;32m"
+# define C_RED		"\033[1;31m"
+# define C_YELLOW	"\033[1;33m"
 
 /*
 ** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,14 +6,43 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 11:26:15 by apuchill          #+#    #+#             */
-/*   Updated: 2021/03/21 15:40:46 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/03/25 23:16:55 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		main(int argc, char *argv[])
+static void	print_prompt(void)
 {
-	ft_printf("Hello, minishell!\n");
-	return (0);
+	char	cwd[PATH_MAX];
+
+	if (getcwd(cwd, PATH_MAX) == NULL)
+		error_msg_and_exit(SYSERR);
+	ft_printf("%s%s➜ %s%s %s✗ %s ", C_GREEN, C_BOLD, C_PINK, cwd, C_YELLOW, C_END);
+}
+
+static void	msh_loop(void)
+{
+	char	*line;
+	char	**args;
+	bool	status;
+
+	status = true;
+	while (status == true)
+	{
+		print_prompt();
+		// line = read_line();
+		// args = split_line(line);
+		// free(line);
+		// status = execute(args);
+		// free(args);
+		status = 0;
+	}
+}
+
+int		main(void)
+{
+	// ignore_signal_for_shell();
+	msh_loop();
+	return (EXIT_SUCCESS);
 }
