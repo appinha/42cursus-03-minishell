3508ec8238d4dfb46243bde3b231645e94318bd1
appinha <amanda_pinha@hotmail.com>
2021-04-03 22:49:04 -0300

    Adds get_environ() - puts env vars into dictionary

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,23 +6,25 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/21 11:26:15 by apuchill          #+#    #+#             */
-/*   Updated: 2021/04/02 15:02:48 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/04/03 22:35:29 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	print_prompt(void)
+static void	print_prompt(t_msh *msh)
 {
 	char	cwd[PATH_MAX];
+	char	*user;
 
 	if (getcwd(cwd, PATH_MAX) == NULL)
 		error_msg_and_exit(SYSERR);
-	ft_printf("%s%s➜ %s%s %s✗ %s ",
-		C_GREEN, C_BOLD, C_PINK, cwd, C_YELLOW, C_END);
+	user = dict_get(msh->dc_environ, "USER");
+	ft_printf("%s%s➜  %s %s%s %s✗ %s ",
+		C_GREEN, C_BOLD, user, C_PINK, cwd, C_YELLOW, C_END);
 }
 
-static void	msh_loop(void)
+static void	msh_loop(t_msh *msh)
 {
 	char	*line;
 	int		status;
@@ -30,18 +32,44 @@ static void	msh_loop(void)
 	status = 0;
 	while (status == 0)
 	{
-		print_prompt();
+		print_prompt(msh);
 		status = get_next_line(STDIN_FILENO, &line);
 		if (status < 0)
 			error_msg_and_exit(SYSERR);
-		ft_printf("%s", line);
 		free(line);
-		ft_printf("%f %i", 100.029, -123809);
 	}
+}
+
+static void	get_environ(t_msh *msh, char **__environ)
+{
+	char	**split;
+	int		i;
+
+	msh->dc_environ = dict_create(ft_strlen_2(__environ));
+	i = 0;
+	while (__environ[i])
+	{
+		split = ft_split(__environ[i], '=');
+		if (split[0] && split[1])
+			dict_insert(msh->dc_environ, split[0], ft_strdup(split[1]));
+		else if (split[0])
+			dict_insert(msh->dc_environ, split[0], ft_strdup(""));
+		ft_split_free(split);
+		i++;
+	}
+}
+
+static void	msh_destroy(t_msh *msh)
+{
+	dict_destroy(msh->dc_environ);
 }
 
 int	main(void)
 {
-	msh_loop();
+	t_msh	msh;
+
+	get_environ(&msh, __environ);
+	msh_loop(&msh);
+	msh_destroy(&msh);
 	return (EXIT_SUCCESS);
 }
