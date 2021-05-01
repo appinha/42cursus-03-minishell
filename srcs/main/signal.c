acdde15964e05c9cbb0d9c284384a4b532f4d005
appinha <amanda_pinha@hotmail.com>
2021-05-01 18:51:10 -0300

    [main] Minor correction in signal handling

--- srcs/main/signal.c
+++ srcs/main/signal.c
@@ -6,14 +6,14 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 11:22:40 by apuchill          #+#    #+#             */
-/*   Updated: 2021/04/27 09:33:26 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/04/28 21:27:53 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
-** Signal handler functions. More info: man signal.
+** Signal handler functions. More info: `man signal`.
 **
 ** SIGINT: program interrupt signal (ctrl + C).
 ** SIGQUIT: (ctrl + \).
@@ -24,8 +24,9 @@ void	sig_prompt(int signum)
 {
 	if (signum == SIGINT)
 	{
-		ft_printf("\n");
+		ft_printf("^C\n");
 		set_exit_status(130);
+		free_null(g_msh.cmd_line);
 		print_prompt(ft_getenv("USER"));
 	}
 	if (signum == EOT)
