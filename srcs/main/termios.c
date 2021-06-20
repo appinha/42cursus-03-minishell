b749f7e05d3be2c0612195666ba7017e2d4b76e0
appinha <amanda_pinha@hotmail.com>
2021-06-20 19:10:21 -0300

    [main][termios] Revises naming used for identifying arrows

--- srcs/main/termios.c
+++ srcs/main/termios.c
@@ -6,7 +6,7 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 21:08:47 by apuchill          #+#    #+#             */
-/*   Updated: 2021/05/09 12:54:03 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/06/20 19:09:20 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,13 @@ void	term_backspace(t_stream *stream, int max_len, int col)
 static void	term_arrow(t_stream *stream, char arrow)
 {
 	if (!g_msh.history
-		|| (arrow == 'A' && stream->is_history == true && !g_msh.hist_curr->prev)
-		|| (arrow == 'B' && stream->is_history == false))
+		|| (arrow == 'U' && stream->is_history == true && !g_msh.hist_curr->prev)
+		|| (arrow == 'D' && stream->is_history == false))
 		return ;
-	if (arrow == 'A' && stream->is_history == false)
+	if (arrow == 'U' && stream->is_history == false)
 		stream->tmp_input = strdup_ver(stream->cmd_line);
 	term_clear_line(stream, ft_strlen(stream->cmd_line), tgetnum("col"));
-	if (arrow == 'B' && stream->is_history == true && !g_msh.hist_curr->next)
+	if (arrow == 'D' && stream->is_history == true && !g_msh.hist_curr->next)
 	{
 		free_null((void **)&stream->cmd_line);
 		stream->cmd_line = stream->tmp_input;
@@ -66,9 +66,9 @@ static void	term_arrow(t_stream *stream, char arrow)
 	}
 	else
 	{
-		if (arrow == 'A' && stream->is_history == true)
+		if (arrow == 'U' && stream->is_history == true)
 			g_msh.hist_curr = g_msh.hist_curr->prev;
-		if (arrow == 'B')
+		if (arrow == 'D')
 			g_msh.hist_curr = g_msh.hist_curr->next;
 		free_null((void **)&stream->cmd_line);
 		stream->cmd_line = strdup_ver(g_msh.hist_curr->cmd_line);
@@ -107,9 +107,9 @@ int	terminal_handler(char *termtype, t_stream *stream, char *buf)
 	else if (buf[0] == DEL)
 		term_backspace(stream, max_len, col);
 	else if (ft_strncmp(buf, ARROW_UP, 3) == 0)
-		term_arrow(stream, 'A');
+		term_arrow(stream, 'U');
 	else if (ft_strncmp(buf, ARROW_DO, 3) == 0)
-		term_arrow(stream, 'B');
+		term_arrow(stream, 'D');
 	else
 		term_get_char(stream, max_len, col, buf[0]);
 	return (0);
